53f3f9c1acb992610691655c838160ae038c136f
Henrichy <Henrynzekwe25@gmail.com>
2022-12-09 12:47:11 -0800

    UUU

--- getLine.c
+++ getLine.c
@@ -50,7 +50,7 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
  * @info: parameter struct
  *
  * Return: bytes read
- *
+ */
 ssize_t get_input(info_t *info)
 {
 	static char *buf; /* the ';' command chain buffer */
