a065442f434a9e700bcfdc091443e38e1e74a62a
Henrichy <Henrynzekwe25@gmail.com>
2022-12-21 22:19:31 -0800

    UUU

--- realloc.c
+++ realloc.c
@@ -14,7 +14,6 @@ char *_memset(char *s, char b, unsigned int n)
 	for (i = 0; i < n; i++)
 		s[i] = b;
 	return (s);
-
 }
 
 /**
@@ -22,28 +21,22 @@ char *_memset(char *s, char b, unsigned int n)
  * @pp: string of strings
  */
 void ffree(char **pp)
-
-
+{
 	char **a = pp;
 
 	if (!pp)
 		return;
 	while (*pp)
-
 		free(*pp++);
-
 	free(a);
-
 }
 
 /**
  * _realloc - reallocates a block of memory
  * @ptr: pointer to previous malloc'ated block
-
  * @old_size: byte size of previous block
  * @new_size: byte size of new block
  *
-
  * Return: pointer to da ol'block nameen.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
@@ -52,7 +45,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (!ptr)
 		return (malloc(new_size));
-
 	if (!new_size)
 		return (free(ptr), NULL);
 	if (new_size == old_size)
--- tokenizer.c
+++ tokenizer.c
@@ -1,11 +1,14 @@
 #include "shell.h"
 
+
+
 /**
  * **strtow - splits a string into words. Repeat delimiters are ignored
  * @str: the input string
  * @d: the delimeter string
  * Return: a pointer to an array of strings, or NULL on failure
  */
+
 
 char **strtow(char *str, char *d)
 {
@@ -48,6 +51,7 @@ char **strtow(char *str, char *d)
 	return (s);
 }
 
+
 /**
  * **strtow2 - splits a string into words
  * @str: the input string
@@ -59,11 +63,9 @@ char **strtow2(char *str, char d)
 	int i, j, k, m, numwords = 0;
 	char **s;
 
-	if (str = NULL || str[0] == 0)
-
-	return (NULL);
-
-for (i = 0; str[i] != '\0'; i++)
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
 		if ((str[i] != d && str[i + 1] == d) ||
 		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
 			numwords++;
